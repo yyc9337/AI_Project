641f168cb9b2bd7d99171df65b0118ea03ff4586
yyc9337 <yyk7009@innerwave.co.kr>
2022-03-03 17:08:51 +0900

    3개 모델 추가

---
+++ [Company] Channel AI (자연어처리)/Library/PyKoSpacing-master/PyKoSpacing-master/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+setup(name='pykospacing',
+      python_requires='>=3.6',
+      version=0.5,
+      url='https://github.com/haven-jeon/PyKoSpacing',
+      license='GPL-3',
+      author='Heewon Jeon',
+      author_email='madjakarta@gmail.com',
+      description='Python package for automatic Korean word spacing.',
+      packages=['pykospacing', ],
+      long_description=open('README.md', encoding='utf-8').read(),
+      zip_safe=False,
+      include_package_data=True,
+
+      install_requires=[
+          'tensorflow == 2.5.3',
+          'h5py == 3.1.0',
+          'argparse >= 1.4.0',
+      ],
+
+      entry_points={
+          'console_scripts': [
+              'pykos = pykospacing.pykos:main',
+          ],
+      },
+      )
